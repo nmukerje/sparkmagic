3412a7d256c4a3d7fe9c6229804313c0bcbcf30c
nmukerje <nmukerje@gmail.com>
2021-06-03 01:07:22 -0700

    Update kernel.js

--- sparkmagic/sparkmagic/kernels/pysparkkernel/kernel.js
+++ sparkmagic/sparkmagic/kernels/pysparkkernel/kernel.js
@@ -1,6 +1,10 @@
 define(['base/js/namespace'], function(IPython){
+        console.log ("debug : onload")
         var onload = function() {
-            IPython.CodeCell.config_defaults.highlight_modes['magic_text/x-sql'] = {'reg':[/^%%sql/]};
+            IPython.CodeCell.options_default.highlight_modes['magic_text/x-sql'] = {'reg':[/^%%sql/]};
+            IPython.CodeCell.options_default.highlight_modes['text/x-python'] = {'reg':[/^%%python/]};
+            IPython.CodeCell.options_default.highlight_modes['text/x-scala'] = {'reg':[/^%%scalaspark/]};
+            IPython.CodeCell.options_default.highlight_modes['text/x-rsrc'] = {'reg':[/^%%sparkr/]};
         }
 
         return { onload: onload }
